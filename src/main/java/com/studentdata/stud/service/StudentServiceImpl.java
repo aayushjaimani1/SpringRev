34a641ec8a74e4b538ab834d80746f62568785a6
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-06-24 17:21:56 +0530

    service implemented

---
+++ src/main/java/com/studentdata/stud/service/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.studentdata.stud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studentdata.stud.model.Student;
+import com.studentdata.stud.repository.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student savesStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    
+}
