63157be0d1d40e724fd895b7d747e76e0527efff
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-06-27 17:31:10 +0530

    model

--- src/main/java/com/studentdata/stud/model/Student.java
+++ src/main/java/com/studentdata/stud/model/Student.java
@@ -5,11 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+// @Entity
 public class Student {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String address;
