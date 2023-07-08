ef9a952fa86c4aa501c26b2424b2ad8c77916602
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-07-08 18:26:14 +0530

    spring integerated

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -23,3 +23,51 @@ export default function Student() {
             console.log("New Student added")
         })
     }
+
+    React.useEffect(()=>{
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setStudents(result);
+        })
+    },[])
+
+
+  return (
+
+    <Container>
+        <Paper elevation ={3} style={paperStyle}>
+            <h1 style={{color:"blue"}}><i>Add Student</i></h1>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleClick}>Submit</Button>
+    </Box>
+    </Paper>
+    <h1>Students</h1>
+    <Paper elevation={3} style={paperStyle}>
+    {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+         ID:{student.id}<br/>
+         Name:{student.name}<br/>
+         Address:{student.address}
+        </Paper>
+    ))}
+
+    </Paper>
+    </Container>
+  );
+}
