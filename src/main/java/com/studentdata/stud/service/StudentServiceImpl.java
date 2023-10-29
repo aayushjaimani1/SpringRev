daebc04a89b8ade0da71ffd1a3365d025236d37b
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-10-29 13:17:18 +0530

    service modified

--- src/main/java/com/studentdata/stud/service/StudentServiceImpl.java
+++ src/main/java/com/studentdata/stud/service/StudentServiceImpl.java
@@ -15,10 +15,10 @@ public class StudentServiceImpl implements StudentService{
 
     @Autowired
     private StudentRepository studentRepository;
-    // @Override
-    // public Student savesStudent(Student student) {
-    //     return studentRepository.save(student);
-    // }
+    @Override
+    public Student savesStudent(Student student) {
+        return studentRepository.save(student);
+    }
     @Override
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
